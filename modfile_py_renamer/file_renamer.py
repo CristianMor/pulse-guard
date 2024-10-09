e55a6807e0252a33f7f06fcc7d59ee8ecdda8510
CristianMor <cristianmor706@gmail.com>
2024-10-09 10:55:03 -0700

    ✨ feat: rename logic file

---
+++ modfile_py_renamer/file_renamer.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import re
+from rich import print
+
+def rename_files(directory):
+
+    pattern = re.compile(r"(\d{3})_(\d{10})_(.*?)_upd\.txt")
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            match = pattern.match(filename)
+            if match:
+                # Tomando los primeros 8 dígitos del segundo grupo (10 dígitos)
+                new_name = f"{match.group(1)}_{match.group(2)[:8]}_{match.group(3)}.txt"
+
+                if new_name != filename:
+                    old_path = os.path.join(directory, filename)
+                    new_path = os.path.join(directory, new_name)
+                    try:
+                        os.rename(old_path, new_name)
+                        print(f"[green]Renombrado exitoso:[/green] {filename} -> [bold yellow]{new_name}[/bold yellow]")
+                    except Exception as e:
+                        print(f"[red]Error al renombrar:[/red] {str(e)}")
+                else:
+                    print(f"[blue]Sin cambios:[/blue] {filename}")
+            else:
+                print(f"[red]No coincide con el patrón:[/red] {filename}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: python modfile-renamer.py /ruta/del/directorio")
+        sys.exit(1)
+
+    directory = sys.argv[1]
+    
+    if not os.path.isdir(directory):
+        print(f"Error: '{directory}' no es un directorio válido.")
+        sys.exit(1)
+
+    rename_files(directory)
+    print("Proceso de renombrado completado.")
